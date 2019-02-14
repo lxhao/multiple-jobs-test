6df2892b793a4f003e908da46d47dff964be067e
Ying Mao <giggle.my@gmail.com>
2019-02-14 15:51:59 -0500

    run multiple jobs concurrently

---
+++ shell/test-wordcount-hibench.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /yarn-test/
+HiBench/bin/workloads/micro/wordcount/prepare/prepare.sh
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+HiBench/bin/workloads/micro/wordcount/hadoop/run.sh
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "Test cost time:"$((end_seconds-start_seconds))"s"
---
+++ test-capacity.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd shell
+./capacity.sh
+/yarn-test/start.sh
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+echo "Test cost time:"$((end_seconds-start_seconds))"s"
+./test-wordcount.sh&
+./test-wordcount2.sh&
+./test-grep.sh&
+./test-logstat.sh&
+./test-logstat2.sh&
+./test-logstat3.sh&
+./test-wordcount-hibench.sh&
+./test-sort-hibench.sh&
+wait
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "======> [CAPACITY] All Test cost time:"$((end_seconds-start_seconds))"s"
+/yarn-test/stop.sh
